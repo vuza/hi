7296c149d16b0f6434a246ce7a8b6a7e8217d140
Marlon Alagoda <marlon@alagoda.at>
2016-03-08 00:23:34 +0100

    created socket io p2p umd export version

---
+++ public/scripts/client.js
@@ -0,0 +1,14 @@
+define(['io', 'P2P'], function(io, P2P){
+    return {
+        run: function(){
+            var socket = io(); //TODO set server address
+            var p2p = new P2P(socket);
+
+            console.log(p2p);
+
+            p2p.on('peer-msg', function (data) {
+                console.log('From a peer %s', data);
+            });
+        }
+    };
+});
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('hi');
+require.config({
+    baseUrl: '/scripts/',
+    paths: {
+        io: 'vendor/bower/socket.io-client/socket.io',
+        P2P: 'vendor/socket.io-p2p.umd-export.js'
+    },
+    deps: ['client'],
+    callback: function (client) {
+        client.run();
+    }
+});
