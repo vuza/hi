2a92d8e37e899c0ea3e5096edd9d7e414df75197
Marlon Alagoda <marlon@alagoda.at>
2016-03-08 21:56:42 +0100

    first "HI" between two clients, but still via server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,10 @@
 define(['io', 'P2P'], function(io, P2P){
     return {
         run: function(){
-            var socket = io(); //TODO set server address
-            var p2p = new P2P(socket);
+            var socket = io('http://localhost:3001'); //TODO => config
+            var p2p = new P2P(socket, {}, function(){
+                p2p.emit('peer-msg', 'HI');
+            });
 
             p2p.on('peer-msg', function (data) {
                 console.log('From a peer %s', data);
